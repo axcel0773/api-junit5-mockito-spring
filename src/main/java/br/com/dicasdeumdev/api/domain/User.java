785375e04c9c28bd16f454a1e0c5ee9b3dbc6bf2
axcel0773 <yuji-watanabe1@hotmail.com>
2024-01-29 14:10:55 -0300

    endpoint create para user

--- src/main/java/br/com/dicasdeumdev/api/domain/User.java
+++ src/main/java/br/com/dicasdeumdev/api/domain/User.java
@@ -2,7 +2,9 @@ package br.com.dicasdeumdev.api.domain;
 
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
