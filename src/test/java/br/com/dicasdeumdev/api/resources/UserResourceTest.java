6ae3ebd70a1a0c6e9738aa346f3c27121af0eed7
axcel0773 <yuji-watanabe1@hotmail.com>
2024-01-30 15:14:54 -0300

    teste para endpoint create da classe UserResource implementado

--- src/test/java/br/com/dicasdeumdev/api/resources/UserResourceTest.java
+++ src/test/java/br/com/dicasdeumdev/api/resources/UserResourceTest.java
@@ -89,11 +89,19 @@ class UserResourceTest {
         assertEquals(NAME, response.getBody().get(INDEX).getName());
         assertEquals(EMAIL, response.getBody().get(INDEX).getEmail());
         assertEquals(PASSWORD, response.getBody().get(INDEX).getPassword());
-
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnCreated() {
+
+        when(service.create(any())).thenReturn(user);
+
+        ResponseEntity<UserDTO> response = resource.create(userDTO);
+
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+
+        assertNotNull(response.getHeaders().get("Location"));
     }
 
     @Test
